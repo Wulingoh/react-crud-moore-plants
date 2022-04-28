a8bf269ecf20497ed6cc0c111b4d7eb96e416246
Michaeline Goh <taifen_g@yahoo.com>
2022-04-29 11:53:35 +1200

    Added form component for new user page

--- src/components/admin/CreateUser.js
+++ src/components/admin/CreateUser.js
@@ -2,74 +2,102 @@ import { useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import { API_HOST } from "../../config";
-import { TextField } from "@mui/material";
+import TextField from "@mui/material/TextField";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import Paper from "@mui/material/Paper";
 
 export default function ListUser() {
-    const navigate = useNavigate();
-    const [inputs, setInputs] = useState([]);
-    const handleChange = (event) => {
-        const name = event.target.name;
-        const value = event.target.value;
-        setInputs(values => ({...values, [name] : value}));
-    }
-    const handleSubmit =(event) => {
-        event.preventDefault();
-        axios.post(`${API_HOST}api/users`, inputs)
-        .then(function(response) {
-         console.log(response.data);
-         navigate("/admin/users");
-        })
-        .catch((error) => {
-            if (error.response) {
-              console.log(error.response);
-              console.log("server responded");
-            } else if (error.request) {
-              console.log("network error");
-            } else {
-              console.log(error);
-            }
-          });
-    }
+  const navigate = useNavigate();
+  const [inputs, setInputs] = useState([]);
+  const handleChange = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    setInputs((values) => ({ ...values, [name]: value }));
+  };
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    axios
+      .post(`${API_HOST}api/users`, inputs)
+      .then(function (response) {
+        console.log(response.data);
+        navigate("/admin/users");
+      })
+      .catch((error) => {
+        if (error.response) {
+          console.log(error.response);
+          console.log("server responded");
+        } else if (error.request) {
+          console.log("network error");
+        } else {
+          console.log(error);
+        }
+      });
+  };
 
-    return (
-        <div>
-            <h1>Create User</h1>
-            <form onSubmit={handleSubmit} >
-                <table cellSpacing="10">
-                    <tbody>
-                        <tr>
-                            <th>
-                                <label>Name: </label>
-                            </th>
-                            <td>
-                                <TextField id="outlined-basic" name="name" label="Outlined" variant="outlined" onChange={handleChange} />
-                            </td>
-                        </tr>
-                        <tr>
-                            <th>
-                                <label>Email: </label>
-                            </th>
-                            <td>
-                                <input type="text" name="email" onChange={handleChange} />
-                            </td>
-                        </tr>
-                        <tr>
-                            <th>
-                                <label>Role: </label>
-                            </th>
-                            <td>
-                                <input type="text" name="role" onChange={handleChange} />
-                            </td>
-                        </tr>
-                        <tr>
-                            <td colSpan="2" align="right">
-                                <button>Save</button>
-                            </td>
-                        </tr>
-                    </tbody>
-                </table>
-            </form>
-        </div>
-        
-    );
+  return (
+    <Paper sx={{ width: "100%", overflow: "hidden" }}>
+      <Box
+        sx={{
+          marginTop: 8,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <Typography component="h1" variant="h5">
+          Create New User
+        </Typography>
+        <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
+          <Grid container spacing={2}>
+            <Grid item xs={12}>
+              <TextField
+                autoComplete="name"
+                name="name"
+                required
+                fullWidth
+                id="name"
+                label="First Name"
+                autoFocus
+                onChange={handleChange}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                required
+                fullWidth
+                id="email"
+                label="Email Address"
+                name="email"
+                autoComplete="email"
+                onChange={handleChange}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                required
+                fullWidth
+                name="role"
+                label="Role"
+                type="text"
+                id="role"
+                autoComplete="role"
+                onChange={handleChange}
+              />
+            </Grid>
+          </Grid>
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            sx={{ mt: 3, mb: 2 }}
+          >
+            Create New User
+          </Button>
+        </Box>
+      </Box>
+    </Paper>
+  );
 }
