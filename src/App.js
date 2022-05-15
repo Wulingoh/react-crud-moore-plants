0759a206e9ef74d381ec187ce15422c38f4db88f
Michaeline Goh <taifen_g@yahoo.com>
2022-05-15 20:28:24 +1200

    Changed routes and nav system

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
-import Home from "./components/Home";
+import Shop from "./components/Shop";
 import Admin from "./components/Admin";
 import '@fontsource/oxygen/300.css';
 import '@fontsource/oxygen/400.css';
@@ -16,7 +16,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/admin/*" element={<Admin />} />
-          <Route path="/" element={<Home />} />
+          <Route index element={<Shop />} />
         </Routes>
       </BrowserRouter>
     </div>
