d314d63f7d462d8d00013050566797298ca38c4c
Michaeline Goh <taifen_g@yahoo.com>
2022-05-27 12:06:49 +1200

    simple login authentication

--- api/auth/login.php
+++ api/auth/login.php
@@ -1,38 +1,48 @@
 <?php
 
 include __DIR__ . "/../config.php";
-
+include "json_responses.php";
 $link = new DbConnect();
 $db = $link->Connect();
-$method = $_SERVER['REQUEST_METHOD'] == "POST";
 $data = json_decode(file_get_contents('php://input'));
-$sql = 'SELECT user_id, name, role, password_hash FROM users WHERE email = ?';
+$sql = 'SELECT user_id, name, email, password_hash, role FROM users WHERE email = :email';
 
 if ($stmt = $db->prepare($sql)) {
     // Bind parameters (s = string, i = int, b = blob, etc), in our case the username is a string so we use "s"
-    $stmt->bindParam('s', $_POST['email']);
+    $stmt->bindParam(':email', $data->email);
     $stmt->execute();
     // Store the result so we can check if the account exists in the database.
 
     if ($stmt->rowCount() > 0) {
-        $stmt->bindValue($userId, $name, $passwordHash, $role);
-        $stmt->fetch();
-        // Account exists, now we verify the password.
-        // Note: remember to use password_hash in your registration file to store the hashed passwords.
-        if (password_verify($_POST['password'], $passwordHash)) {
-            // Verification success! User has logged-in!
-            // Create sessions, so we know the user is logged in, they basically act like cookies but remember the data on the server.
-            session_regenerate_id();
-            $_SESSION['loggedin'] = TRUE;
-            $_SESSION['email'] = $_POST['email'];
-            $_SESSION['name'] = $userId;
-            $_SESSION['role'] = $role;
+        if($row = $stmt->fetch()){
+            $userId = $row["user_id"];
+            $name = $row["name"];
+            $email = $row["email"];
+            $passwordHash =$row["password_hash"];
+            $role = $row["role"];
 
-            $stmt->null;
+            if (password_verify($data->password, $passwordHash)) {
+                // Verification success! User has logged-in!
+                // Create sessions, so we know the user is logged in, they basically act like cookies but remember the data on the server.
+                session_regenerate_id();
+                $_SESSION['loggedin'] = TRUE;
+                $_SESSION['id'] = $userId;
+                $_SESSION['email'] = $email;
+                $_SESSION['name'] = $name;
+                $_SESSION['role'] = $role;
+
+                // if($role == "admin") {
+                //     header('Location: admin/index.php');
+                // }
+
+                returnJsonHttpResponse(true, "You have successfully logged in!"); 
+            } else {
+                returnJsonHttpResponse(false, "The password you entered was not valid!");
+            }
         }
     } 
     else {
-        $data = ['status' => 0, 'message' => "Failed to log in."];
+        returnJsonHttpResponse(false, "No account found with that email address!");
     }
 }
 ?>
