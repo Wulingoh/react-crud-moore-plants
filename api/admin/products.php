9e5ff543ed841d95d22a3f2b9cda483a609ae20a
Michaeline Goh <taifen_g@yahoo.com>
2022-06-14 09:57:17 +1200

    Added pot data and fixed products form

--- api/admin/products.php
+++ api/admin/products.php
@@ -48,11 +48,11 @@
         case 'GET':
             $sql = "SELECT products.product_id, products.category_id, category.name as categoryName, products.type, products.img, products.title, products.name, products.price, products.quantity, products.color, products.height, products.latin_name, products.lighting_care_id,lighting_care.name as lightingName, products.care_level_id, care_level.name as careLevelName, products.watering_id,watering.name as wateringName, products.humidity_id,humidity.name as humidityName, products.room_type, products.size, products.pot_material, products.content, products.facts 
             FROM products
-            INNER JOIN category on products.category_id = category.category_id 
-            INNER JOIN lighting_care on products.lighting_care_id = lighting_care.lighting_id 
-            INNER JOIN care_level on products.care_level_id = care_level.care_level_id 
-            INNER JOIN watering on products.watering_id = watering.watering_id 
-            INNER JOIN humidity on products.humidity_id = humidity.humidity_id ";
+            LEFT JOIN category on products.category_id = category.category_id 
+            LEFT JOIN lighting_care on products.lighting_care_id = lighting_care.lighting_id 
+            LEFT JOIN care_level on products.care_level_id = care_level.care_level_id 
+            LEFT JOIN watering on products.watering_id = watering.watering_id 
+            LEFT JOIN humidity on products.humidity_id = humidity.humidity_id ";
             $path = explode('/', $_SERVER['REQUEST_URI']);
             if(isset($path[4]) && is_numeric($path[4])) {
                 $sql .= " WHERE product_id = :productId";
